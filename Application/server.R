93e637d9d9aaf126ac4ed2c6569f7a4d711493ac
cbirkho <connor_birkhold@gap.com>
2020-01-08 14:55:39 -0800

    updated

--- Application/server.R
+++ Application/server.R
@@ -5,75 +5,76 @@ library(RSQLite)
 library(DBI)
 library(leaflet)
 
-shinyServer(function(input, output){
+shinyServer(function(input, output, session){
     
-    # Query the data from the database
-    dbPath <- "./sf_crime_db.sqlite"
-    db <- dbConnect(RSQLite::SQLite(), dbname = dbPath)
-    
-    sfCrimeMap <- dbGetQuery(db, "SELECT 
-                                        sum(incident_cnt) as cnt,
-                                        latitude,
-                                        longitude,
-                                        police_district,
-                                        incident_category
-                                    FROM incident_reports
-                                    GROUP BY
-                                        latitude,
-                                        longitude,
-                                        police_district,
-                                        incident_category;")
-    
-    dbDisconnect(db)  # disconnect from db
-    
-    # create categoryChart
-    output$categoryChart <- renderPlotly({
+    observeEvent(input$overviewTabs, {
         
+        if(input$overviewTabs == "overviewTab2"){
+            # Code for Interactive Map tab
+            
+            # reactive expression to get/change data
+            sfCrimeFiltMap <- reactive({
+                
+                dbPath <- "./sf_crime_db.sqlite"
+                db <- dbConnect(RSQLite::SQLite(), dbname = dbPath)
+                
+                sfCrimeMap <- dbGetQuery(db, "SELECT 
+                                                sum(incident_cnt) as cnt,
+                                                latitude,
+                                                longitude,
+                                                police_district,
+                                                incident_category
+                                            FROM incident_reports
+                                            GROUP BY
+                                                latitude,
+                                                longitude,
+                                                police_district,
+                                                incident_category;")
+                
+                dbDisconnect(db)
+                
+                if(input$districtInput == "All"){
+                    sfCrimeFiltMap <- sfCrimeMap
+                } else {
+                    sfCrimeFiltMap <- filter(sfCrimeMap, police_district == input$districtInput)
+                }
+                
+                sfCrimeFiltMap
+            })
+            
+            # create overviewMap
+            output$overviewMap <- renderLeaflet({
+                
+                pal <- colorFactor(topo.colors(11), sfCrimeFiltMap()$police_district)
+                
+                sfCrimeFiltMap() %>%
+                    leaflet() %>%
+                    addTiles() %>%
+                    addCircles(weight = 12,
+                               radius = sqrt(sfCrimeFiltMap()$cnt) * 30,
+                               color = ~ pal(police_district),
+                               popup = ~ incident_category) %>%
+                    addMarkers(clusterOptions = markerClusterOptions()) %>%
+                    addLegend(pal = pal, values = ~ police_district,
+                              position = "topleft", title = "District")
+            })
+            
+            # create histMap
+            output$barChartMap <- renderPlot({
+                
+                ggplot(sfCrimeFiltMap(), aes(x = police_district, y = cnt)) +
+                    geom_bar(stat = 'identity', fill = 'green', alpha = 0.5) +
+                    theme_classic() +
+                    theme(axis.text.x = element_text(size = 14, angle = 90),
+                          axis.text.y = element_text(size = 10),
+                          plot.title = element_text(hjust = 0.5)) +
+                    ggtitle("Criminal Offenses") +
+                    xlab(element_blank()) +
+                    ylab("Count")
+                
+            })
+        } 
     })
     
-    
-    # create overviewMap
-    output$overviewMap <- renderLeaflet({
-        
-        if(input$districtInput == "All"){
-            sfCrimeFiltMap <- sfCrimeMap
-        } else {
-            sfCrimeFiltMap <- filter(sfCrimeMap, police_district == input$districtInput)
-        }
-        
-        pal <- colorFactor(topo.colors(11), sfCrimeMap$police_district)
-        
-        sfCrimeFiltMap %>% 
-            leaflet() %>% 
-            addTiles() %>% 
-            addCircles(weight = 12, 
-                       radius = sqrt(sfCrimeFiltMap$cnt) * 30,
-                       color = ~ pal(police_district),
-                       popup = ~ incident_category) %>% 
-            addMarkers(clusterOptions = markerClusterOptions()) %>% 
-            addLegend(pal = pal, values = ~ police_district, 
-                      position = "topleft", title = "District")
-    })
-    
-    # create histMap
-    output$barChartMap <- renderPlot({
-        
-        if(input$districtInput == "All"){
-            sfCrimeFiltMap <- sfCrimeMap
-        } else {
-            sfCrimeFiltMap <- filter(sfCrimeMap, police_district == input$districtInput)
-        }
-        
-        ggplot(sfCrimeFiltMap, aes(x = police_district, y = cnt)) +
-            geom_bar(stat = 'identity', fill = 'green', alpha = 0.5) +
-            theme_classic() +
-            theme(axis.text.x = element_text(size = 14, angle = 90),
-                  axis.text.y = element_text(size = 10),
-                  plot.title = element_text(hjust = 0.5)) +
-            ggtitle("Criminal Offenses") +
-            xlab(element_blank()) +
-            ylab(element_blank())
-        
-    })
     
 })
--- Application/ui.R
+++ Application/ui.R
@@ -7,84 +7,68 @@ library(plotly)
 shinyServer(navbarPage(
     "San Francisco Criminal Offenses",
     
-    theme = shinytheme("sandstone"),
+    theme = shinytheme("superhero"),
     
     tabPanel("Overview",
-            mainPanel(
-                tabsetPanel(
-                    tabPanel("Summary Statistics", 
-                             
-                             sidebarPanel(width = 3,
+             sidebarLayout(
+                 sidebarPanel(width = 3,
+                              
+                              selectInput("districtInput", 'District:',
+                                          list('All',
+                                               'Central',
+                                               'Richmond',
+                                               'Mission',
+                                               'Taraval',
+                                               'Ingleside',
+                                               'Southern',
+                                               'Bayview',
+                                               'Northern',
+                                               'Tenderloin',
+                                               'Park')),
+                              
+                              sliderInput("dateSlider", "Date:",
+                                          min = as.Date("2018-01-01", "%Y-%m-%d"),
+                                          max = as.Date("2020-01-01", "%Y-%m-%d"),
+                                          value = as.Date("2018-01-01"))
+                 ),
+                 
+                 mainPanel(
+                     tabsetPanel(type = 'tabs', id = "overviewTabs",
                                  
-                                 selectInput("districtInput", 'District:',
-                                             list('All',
-                                                  'Central',
-                                                  'Richmond',
-                                                  'Mission',
-                                                  'Taraval',
-                                                  'Ingleside',
-                                                  'Southern',
-                                                  'Bayview',
-                                                  'Northern',
-                                                  'Tenderloin',
-                                                  'Park')),
+                                 tabPanel(title = "Summary Statistics", value = 'overviewTab1',
+                                          
+                                          
+                                 ),
                                  
-                                 sliderInput("dateSlider", "Date:",
-                                             min = as.Date("2018-01-01", "%Y-%m-%d"),
-                                             max = as.Date("2020-01-01", "%Y-%m-%d"),
-                                             value = as.Date("2018-01-01"))
-                             ),
-                             
-                             mainPanel(
-                                 fluidRow(
-                                     column(8, ),
-                                     column(12, plotlyOutput("categoryChart"))
+                                 tabPanel(title = "Interactive Map", value = 'overviewTab2',
+                                          
+                                          # include custom CSS
+                                          div(class='outer',
+                                              
+                                              tags$head(
+                                                  
+                                                  includeCSS("styles.css"),
+                                              ),
+                                              
+                                          ),
+                                          
+                                          leafletOutput("overviewMap", height = 1000, width = 1800),
+                                          
+                                          absolutePanel(id = 'controls', class = "panel panel-default",
+                                                        fixed = TRUE, draggable = TRUE, top = 'auto',
+                                                        left = 555, right = 'auto', bottom = 150,
+                                                        width = 330, height = 'auto',
+                                                        
+                                                        plotOutput("barChartMap", height = 400)
+                                          ),
+                                          
+                                          # tags$div(id="cite",
+                                          #          'Data was refreshed on ', tags$em('2020-01-07'))
                                  )
-                             )
-                    ),
-                    
-                    
-                    tabPanel("Interactive Map", 
-                             
-                             div(class='outer',
-                                 
-                                 tags$head(
-                                     # include custom CSS
-                                     includeCSS("styles.css"),
-                                 ),
-                                 
-                                 ),
-                             
-                             leafletOutput("overviewMap", height = 850, width = 1900),
-                             
-                             absolutePanel(id = 'controls', class = "panel panel-default",
-                                           fixed = TRUE, draggable = TRUE, top = 150,
-                                           left = 'auto', right = 20, bottom = 'auto',
-                                           width = 330, height = 'auto',
-                                           
-                                           h2("District Explorer"),
-                                           
-                                           selectInput("districtInput", 'District:',
-                                                       list('All',
-                                                            'Central',
-                                                            'Richmond',
-                                                            'Mission',
-                                                            'Taraval',
-                                                            'Ingleside',
-                                                            'Southern',
-                                                            'Bayview',
-                                                            'Northern',
-                                                            'Tenderloin',
-                                                            'Park')),
-                                           
-                                           plotOutput("barChartMap", height = 400)
-                             ),
-                             
-                             # tags$div(id="cite",
-                             #          'Data was refreshed on ', tags$em('2020-01-07'))
-                    )
-                )
-            )
+                     )
+                 )
+             ),
+             
     ),
     
     tabPanel("Inferential Stats",
@@ -92,12 +76,12 @@ shinyServer(navbarPage(
     ),
     
     tabPanel("Predictions",
-    
+             
     ),
     
     navbarMenu("More",
                tabPanel("Data Explorer",
-               
+                        
                ),
                tabPanel("Your Data",
                         
